4ee1d9b00977131a326dda2b38a3aba5e45ad5c1
bunchley <bunchley007@gmail.com>
2019-11-04 07:53:15 -0700

    added grid

--- script/main.js
+++ script/main.js
@@ -1,17 +1,16 @@
-
 //generate grid
-var gridPlace = document.querySelector('.grid-container');
+var gridPlace = document.querySelector(".grid-container");
 let count = 1;
 
-for(i =0; i<16; i++){
-    let row = document.createElement("div");
-    row.classList.add("row");
-    row.textContent = count++;
-    gridPlace.appendChild(row);
-    for(j =0; j<15; j++){
-        let cell = document.createElement("div");
-        cell.className ="cell";
-        cell.textContent = count++;
-        row.appendChild(cell);
-    }
+for (i = 0; i < 16; i++) {
+  let row = document.createElement("div");
+  row.classList.add("row");
+  row.textContent = count++;
+  gridPlace.appendChild(row);
+  for (j = 0; j < 15; j++) {
+    let cell = document.createElement("div");
+    cell.className = "cell";
+    cell.textContent = `| ${count++} |`;
+    row.appendChild(cell);
+  }
 }
