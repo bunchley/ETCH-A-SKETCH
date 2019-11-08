52ec5eeddd99b6fe2e7a2d21198d32a9425d3b2b
bunchley <bunchley007@gmail.com>
2019-11-08 16:03:14 -0700

    grid and shading features

--- script/main.js
+++ script/main.js
@@ -1,16 +1,25 @@
 //generate grid
-var gridPlace = document.querySelector(".grid-container");
-let count = 1;
+const gridPlace = document.querySelector(".grid-container");
 
-for (i = 0; i < 16; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  row.textContent = count++;
-  gridPlace.appendChild(row);
-  for (j = 0; j < 15; j++) {
+let count = 1;
+let gridSize = 16;
+
+for (i = 0; i < gridSize; i++) {
+  for (j = 0; j < gridSize; j++) {
     let cell = document.createElement("div");
     cell.className = "cell";
     cell.textContent = `| ${count++} |`;
-    row.appendChild(cell);
+    gridPlace.appendChild(cell);
+    cell.addEventListener("mouseover", addColor);
   }
+}
+
+const createGrid = gridSize => {
+  gridPlace.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  gridPlace.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+};
+
+function addColor(event) {
+  // let cell = document.querySelector(".cell");
+  event.target.classList.add("colored");
 }
