8a71ae0e7ebfd96bfab1a9961c10f394d3848212
Ashley Bunch <bunchley007@gmail.com>
2024-12-28 11:20:12 -0700

    added grid in flex and shading when hovered

--- script/main.js
+++ script/main.js
@@ -1,20 +1,25 @@
 //generate grid
 const gridPlace = document.querySelector(".grid-container");
-
+// const
 let count = 1;
 let gridSize = 16;
 
 for (i = 0; i < gridSize; i++) {
+  let col = document.createElement("div");
+  col.className = `col ${i}`;
+  gridPlace.appendChild(col);
+
   for (j = 0; j < gridSize; j++) {
     let cell = document.createElement("div");
     cell.className = "cell";
-    cell.textContent = `| ${count++} |`;
-    gridPlace.appendChild(cell);
+    cell.className = `row ${j}`;
+    cell.textContent = `${count++}`;
     cell.addEventListener("mouseover", addColor);
+    col.appendChild(cell);
   }
 }
 
-const createGrid = gridSize => {
+const createGrid = (gridSize) => {
   gridPlace.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   gridPlace.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 };
