29f585155774726ea145895c83bfaf499bcef637
Ashley Bunch <bunchley007@gmail.com>
2024-12-28 20:25:01 -0700

    added reset grid size button and functionality

--- script/main.js
+++ script/main.js
@@ -1,30 +1,34 @@
 //generate grid
 const gridPlace = document.querySelector(".grid-container");
-// const
+const resetButton = document.querySelector(".reset-button");
 let count = 1;
 let gridSize = 16;
 
-for (i = 0; i < gridSize; i++) {
-  let col = document.createElement("div");
-  col.className = `col ${i}`;
-  gridPlace.appendChild(col);
+function createGrid(gridSize = 16) {
+  for (i = 0; i < gridSize; i++) {
+    let col = document.createElement("div");
+    col.className = `col ${i}`;
+    gridPlace.appendChild(col);
 
-  for (j = 0; j < gridSize; j++) {
-    let cell = document.createElement("div");
-    cell.className = "cell";
-    cell.className = `row ${j}`;
-    cell.textContent = `${count++}`;
-    cell.addEventListener("mouseover", addColor);
-    col.appendChild(cell);
+    for (j = 0; j < gridSize; j++) {
+      let cell = document.createElement("div");
+      cell.className = "cell";
+      cell.className = `row ${j}`;
+      cell.textContent = `  `;
+      cell.addEventListener("mouseover", addColor);
+      col.appendChild(cell);
+    }
   }
 }
 
-const createGrid = (gridSize) => {
-  gridPlace.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-  gridPlace.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-};
-
 function addColor(event) {
-  // let cell = document.querySelector(".cell");
   event.target.classList.add("colored");
 }
+
+resetButton.addEventListener("click", (event) => {
+  let newGridSize = prompt("What is the grid size you'd like (max 100)");
+  gridPlace.innerHTML = "";
+  createGrid(newGridSize);
+});
+
+createGrid(16);
